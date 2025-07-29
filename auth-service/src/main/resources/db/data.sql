0d78769ee2f549de43a35d685441e821f3f4c430
arismetyogi <arismetyogi@gmail.com>
2025-07-29 12:08:57 +0700

    fix: updated data.sql field insert default password

--- auth-service/src/main/resources/db/data.sql
+++ auth-service/src/main/resources/db/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO users (name, username, email, password, is_active, last_login, role, created_at)
 VALUES
-    ('Admin One',  'admin', 'admin@example.com', '$2a$10$7W8e5Tk4a.uQk5b.qZoeLO9Llb/2L8W/3h1V5s8MTb7Hv7AFR1m2G', TRUE, NOW(), 'ADMIN', NOW()),
-    ('User One',   'user1', 'user1@example.com', '$2a$10$7W8e5Tk4a.uQk5b.qZoeLO9Llb/2L8W/3h1V5s8MTb7Hv7AFR1m2G', TRUE, NULL,  'USER', NOW()),
-    ('User Two',   'user2', 'user2@example.com', '$2a$10$7W8e5Tk4a.uQk5b.qZoeLO9Llb/2L8W/3h1V5s8MTb7Hv7AFR1m2G', TRUE, NULL,  'USER', NOW());
+    ('Admin One',  'admin', 'admin@example.com', '$2a$12$qGoDWkqsOGRmHG.VyG/zGOVIF3nLUhqy4BXw/wpKkLjikXStdmAWq', TRUE, NOW(), 'ADMIN', NOW()),
+    ('User One',   'user1', 'user1@example.com', '$2a$12$qGoDWkqsOGRmHG.VyG/zGOVIF3nLUhqy4BXw/wpKkLjikXStdmAWq', TRUE, NULL,  'USER', NOW()),
+    ('User Two',   'user2', 'user2@example.com', '$2a$12$qGoDWkqsOGRmHG.VyG/zGOVIF3nLUhqy4BXw/wpKkLjikXStdmAWq', TRUE, NULL,  'USER', NOW());
 
+-- default password: "password"
